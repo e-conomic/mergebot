7d21ad50b31ea6c8232bd584c6c0831d95d3511f
Mihăiţă Bamburic <mihaita.bamburic@visma.com>
2021-04-19 17:13:43 +0300

    Update src/services/pullRequestService.ts
    Co-authored-by: Nicolai Brogaard <nicolai.brogaard@visma.com>

--- src/services/pullRequestService.ts
+++ src/services/pullRequestService.ts
@@ -2,11 +2,11 @@ import { InternalContext } from '../models/actionContextModels'
 import { PullRequestModel } from '../models/gitHubModels'
 
 function shouldMergePr (pullRequest: PullRequestModel, internalContext: InternalContext): boolean {
-  if (pullRequest.labels.some(label => label.name !== internalContext.input.label)) {
-    return false
+  if (pullRequest.labels.some(label => label.name === internalContext.input.label)) {
+    return true
   }
 
-  return true
+  return false
 }
 
 export {
